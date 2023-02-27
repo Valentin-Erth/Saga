f4075c5beb5ed91f3fc6c52d25e3638dd6116242
Kozlov Valentin <kozlov0020@gmail.com>
2023-02-27 14:32:27 +0500

    creat new arr:study axios.get, swagger, useState, map, useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
 import './App.css';
 
+export type PhotoSizeViewModel= {
+    url: string
+    width: number
+    height: number
+    fileSize: number
+}
+
+export type TodoListImagesViewModel ={
+    main: PhotoSizeViewModel[]
+}
+
+export type TodoListViewModel ={
+    isImportant: boolean
+    id: string
+    title: string
+    description: string
+    addedDate: Date
+    order: number
+    images: TodoListImagesViewModel
+}
+
+export type GetTotdoListResponseType ={
+    pagesCount: number
+    page: number
+    pageSize: number
+    totalCount: number
+    items: TodoListViewModel[]
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    //1.tags-[]
+    //6.tags=[{},{},{},{}]// tags from step 5
+    const [todolist, setTodolist] = useState<GetTotdoListResponseType|null>(null/*1*/)
+
+    //2 register effect
+    useEffect(() => {
+        //4.run effect
+        console.log("useEffect")
+        axios.get<GetTotdoListResponseType>("https://todolists.samuraijs.com/api/1.0/todolists")
+            .then(response => {
+            console.log(response.data)
+            setTodolist(response.data)
+        })
+    }, [] /*call effect 1 time*/)
+    //3.emty rendering
+    //7.tags rendering
+    console.log("rendering")
+    return (
+        <div className="App">
+            <div>
+                {todolist===null
+                    ?"loading"
+                    :todolist.items.map(el => {
+                        const imgesUrl = el.images.main.length > 1 ? el.images.main[1].url : "";
+                        return (
+                            <div key={el.id}>
+                                <img src={imgesUrl} alt=""/>
+                                <h3>
+                                    {!el.isImportant ? "🔥" : ""}
+                                    {/*<input type={"checkbox"} checked={!el.isImpotant}/>*/}
+                                    {el.title}</h3>
+                                <div>
+                                    {el.description}
+                                </div>
+                            </div>
+                        )
+                    }
+                )}
+            </div>
+        </div>
+    );
 }
 
 export default App;
